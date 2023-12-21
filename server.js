4ad5f4f1911858bbf34e4a844343c8e849acfb48
Kristof Dombi <dombi.kristof@gmail.com>
2023-12-21 15:24:22 +0100

    chore(kinsta): add splash screen

--- server.js
+++ server.js
@@ -1,43 +1,49 @@
-require('dotenv').config()
-const express = require('express')
-const sgMail = require("@sendgrid/mail");
+require("dotenv").config()
+const express = require("express")
+const path = require("path")
+const sgMail = require("@sendgrid/mail")
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const app = express()
 
-if(!process.env.TEST_EMAIL_TO_ADDRESS || !process.env.TEST_EMAIL_FROM_ADDRESS || !process.env.SENDGRID_API_KEY || !process.env.TEST_ENDPOINT) {
-    console.log("Make sure to set the following environment variables for your application:\n‣ TEST_EMAIL_TO_ADDRESS\n‣ TEST_EMAIL_FROM_ADDRESS\n‣ SENDGRID_API_KEY\n‣ TEST_ENDPOINT\n")
-    process.exit()
+if (
+  !process.env.TEST_EMAIL_TO_ADDRESS ||
+  !process.env.TEST_EMAIL_FROM_ADDRESS ||
+  !process.env.SENDGRID_API_KEY ||
+  !process.env.TEST_ENDPOINT
+) {
+  console.log(
+    "Make sure to set the following environment variables for your application:\n‣ TEST_EMAIL_TO_ADDRESS\n‣ TEST_EMAIL_FROM_ADDRESS\n‣ SENDGRID_API_KEY\n‣ TEST_ENDPOINT\n",
+  )
+  process.exit()
 }
 
-
-app.get('/', async (req, res) => {
-    res.send("Hello World")
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"))
 })
 
 app.get(`/${process.env.TEST_ENDPOINT}`, async (req, res) => {
-    const msg = {
-        to: process.env.TEST_EMAIL_TO_ADDRESS,
-        from: process.env.TEST_EMAIL_FROM_ADDRESS,
-        subject: 'My Test Email',
-        text: 'This is a test email sent by your Node.js application when you visited the test endpoint',
-        html: 'This is a <strong>test</strong> email sent by your Node.js application when you visited the test endpoint',
-    };
+  const msg = {
+    to: process.env.TEST_EMAIL_TO_ADDRESS,
+    from: process.env.TEST_EMAIL_FROM_ADDRESS,
+    subject: "My Test Email",
+    text: "This is a test email sent by your Node.js application when you visited the test endpoint",
+    html: "This is a <strong>test</strong> email sent by your Node.js application when you visited the test endpoint",
+  }
 
-    try {
-        await sgMail.send(msg);
-    } catch (error) {
-        console.error(error);
+  try {
+    await sgMail.send(msg)
+  } catch (error) {
+    console.error(error)
 
-        if (error.response) {
-            console.error(error.response.body)
-        }
+    if (error.response) {
+      console.error(error.response.body)
     }
+  }
 
-    res.send("Email sent")
+  res.send("Email sent")
 })
 
-
 app.listen(process.env.PORT, () => {
-    console.log(`Hello World Application is running on port ${process.env.PORT}`)
+  console.log(`Hello World Application is running on port ${process.env.PORT}`)
 })
